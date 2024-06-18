3d703d2510fde5979b5eac715ffdcc3d3098f378
Sven Widén <sven.widen@gmail.com>
2024-06-18 09:27:06 +0200

    Add possibility for interceptors in the GrpcPods client (#131)
    * Add possibility for interceptors in the GrpcPods client
    
    * Add docs to new config variable

--- protocol-grpc/src/main/scala/com/devsisters/shardcake/GrpcConfig.scala
+++ protocol-grpc/src/main/scala/com/devsisters/shardcake/GrpcConfig.scala
@@ -1,6 +1,7 @@
 package com.devsisters.shardcake
 
 import zio._
+import scalapb.zio_grpc.ZClientInterceptor
 
 import java.util.concurrent.Executor
 
@@ -10,10 +11,16 @@ import java.util.concurrent.Executor
  * @param maxInboundMessageSize the maximum message size allowed to be received by the grpc client
  * @param executor a custom executor to pass to grpc-java when creating gRPC clients and servers
  * @param shutdownTimeout the timeout to wait for the gRPC server to shutdown before forcefully shutting it down
+ * @param interceptors the interceptors to be used by the gRPC client, e.g for adding tracing or logging
  */
-case class GrpcConfig(maxInboundMessageSize: Int, executor: Option[Executor], shutdownTimeout: Duration)
+case class GrpcConfig(
+  maxInboundMessageSize: Int,
+  executor: Option[Executor],
+  shutdownTimeout: Duration,
+  interceptors: Seq[ZClientInterceptor]
+)
 
 object GrpcConfig {
   val default: GrpcConfig =
-    GrpcConfig(maxInboundMessageSize = 32 * 1024 * 1024, None, 3.seconds)
+    GrpcConfig(maxInboundMessageSize = 32 * 1024 * 1024, None, 3.seconds, Seq.empty)
 }
